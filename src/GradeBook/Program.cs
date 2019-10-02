1300614f180a4183b12b9d00fceea8e7f3d0c5c2
tubaShawn <shawnrmangum@gmail.com>
2019-10-01 20:01:06 -0600

    basic syntax practice

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,6 +6,11 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            double x = 34.1;
+            double y = 17.562;
+
+            Console.WriteLine(x + y);
+
             if(args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}");
