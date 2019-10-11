b8c7db2ddc70ef3713f8f25a0453a49d585f832c
tubaShawn <shawnrmangum@gmail.com>
2019-10-10 20:41:27 -0600

    working on gradebook operations

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,13 +9,21 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+
+            var p = new Program();
+            Program.Main(args);
+
             var book = new Book("Shawn's gradebook");
             book.AddGrade(89.1);
             book.AddGrade(90.5);
 
             var grades = new List<double> { 10.0, 11.1, 22.2, 54.3 };
             var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
             foreach(double number in grades) {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
                 result += number;
             }
 
